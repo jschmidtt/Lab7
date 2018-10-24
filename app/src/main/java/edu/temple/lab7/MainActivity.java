34ac94f0b450b3900b9454b996cef877250e3e35
justinschmidt <justinschmidttt@gmail.com>
2018-10-24 09:46:01 -0400

    Home Page URL.

--- app/src/main/java/edu/temple/lab7/MainActivity.java
+++ app/src/main/java/edu/temple/lab7/MainActivity.java
@@ -1,13 +1,62 @@
 package edu.temple.lab7;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fm = getSupportFragmentManager();
+    Button goButton;
+    TextView textViewURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goButton = findViewById(R.id.goButton);
+
+        textViewURL = findViewById(R.id.urlEditText);
+
+        //Main Page Home Page
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WebViewFragment webViewFragment = WebViewFragment.newInstance(textViewURL.getText().toString());
+                fm.beginTransaction().replace(R.id.container_1, webViewFragment).addToBackStack(null).commit();
+            }
+        });
+    }
+
+    //ActionBar---Inflate Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.browser_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //ActionBar---Handling Menu Clicks
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_backward){
+            //Action Backward
+        } else if (id == R.id.action_forward){
+            //Action Forward
+        } else if (id == R.id.action_new){
+            //Action New
+        }
+
+        return super.onContextItemSelected(item);
     }
 }
--- app/src/main/java/edu/temple/lab7/WebViewFragment.java
+++ app/src/main/java/edu/temple/lab7/WebViewFragment.java
@@ -1,29 +1,100 @@
 package edu.temple.lab7;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class WebViewFragment extends Fragment {
 
+    View v;
+    WebView webView;
+    TextView urlTextView;
+    Context context;
+
+    String URL;
+
+    public static String WEB_KEY = "web_key";
 
     public WebViewFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getArguments();
+        URL = bundle.getString(WEB_KEY);
+    }
+
+    public static WebViewFragment newInstance(String URL){
+        //Create instances of the Fragment
+        WebViewFragment webViewFragment = new WebViewFragment();
+
+        //Passing Arguments && Creating Bundle
+        Bundle bundle = new Bundle();
+        bundle.putString(WebViewFragment.WEB_KEY, URL);
+        webViewFragment.setArguments(bundle);
+
+        return webViewFragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web_view, container, false);
+        v = inflater.inflate(R.layout.fragment_web_view, container, false);
+
+        webView = v.findViewById(R.id.webView);
+
+        loadUrlFromTextView();
+
+        return v;
     }
+
+    //URL METHOD
+    private void loadUrlFromTextView() {
+        new Thread(){
+            public void run(){
+                //Grab url from urlTextView
+                String sb = URL;
+                Message msg = Message.obtain();
+                msg.obj = sb;
+                responseHandler.sendMessage(msg);
+            }
+        }.start();
+    }
+
+    //Handler
+    Handler responseHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            Toast.makeText(context, (String) msg.obj, Toast.LENGTH_SHORT).show();
+            webView.loadUrl((String) msg.obj);
+            return false;
+        }
+    });
 
 }
